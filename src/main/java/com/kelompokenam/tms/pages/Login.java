898ddaea42ff8541372ea64576c30e92b1565e84
Abdul Azis Fikri <azisfikriaaf@gmail.com>
2022-11-07 22:32:45 +0700

    User Rizki

--- src/main/java/com/kelompokenam/tms/pages/Login.java
+++ src/main/java/com/kelompokenam/tms/pages/Login.java
@@ -7,6 +7,10 @@ import org.openqa.selenium.support.PageFactory;
 public class Login {
     private WebDriver driver;
 
+    //Rizki
+    //User D6201031
+    //Pass 1994-10-02
+
     public Login() {
         this.driver = DriverSingleton.getDriver();
         PageFactory.initElements(driver, this);
